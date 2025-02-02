aea0e76a803bdfd60d00c32a49992ff154aab213
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2025-02-02 13:37:18 +0100

    product list

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,20 +1,26 @@
-import {Button, InputNumber} from "antd";
+import {Button, Card, InputNumber} from "antd";
 import { PlusOutlined } from '@ant-design/icons';
+import {Meta} from "antd/es/list/Item";
 
 function Product({product}) {
     const {title,price,images,category} = product;
     const image = images[0];
     return (
-        <div className={"productCard"}>
-            <img src={image} height={200} width={200}/>
+        <Card
+            hoverable
+            style={{
+                width: 240,
+            }}
+            cover={<img alt="example" src={image}/>}
+            actions={[
+                <InputNumber min={1} defaultValue={1}/>,
+                <Button type="primary" onClick={() => console.log(product.id)} shape="circle" icon={<PlusOutlined/>}/>,
+            ]}
+        >
             <p className={"price"}>{price}</p>
-            <p className={"title"}>{title}</p>
+            <Meta title={title}/>
             <p className={"categoryName"}>{category.name}</p>
-            <div className={"addToCard"}>
-                <InputNumber min={1}  defaultValue={1}  />
-                <Button type="primary" onClick={() => console.log(product.id)} shape="circle" icon={<PlusOutlined />} />
-            </div>
-        </div>
+        </Card>
     );
 
 }
